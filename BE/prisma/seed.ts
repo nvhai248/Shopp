d640dbeee8dcad1dd4cd325f95ddeb9015c1c05e
nvhai248 <nvhai2408@gmail.com>
2024-06-03 06:57:08 -0400

    [add] seed db

---
+++ BE/prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.hShopAdmin.upsert({
+    where: { email: 'admin@hshopp.icom' },
+    update: {},
+    create: {
+      email: 'admin@hshopp.icom',
+      firstName: 'Hai',
+      lastName: 'Nguyen',
+      gender: 'MALE',
+      phoneNumber: '0813042255',
+      password: '$2b$10$a9Eaf/fML42Kqro/QNd/FewOm.hDdiu3omJHqsh6xQ2Kzz7FiX8Uu',
+      salt: '$2b$10$a9Eaf/fML42Kqro/QNd/Fe',
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
